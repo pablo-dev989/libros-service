be1b2a5691ac1f7538efcfb4e812362fc80dfeea
simon <pablo.ralil.dev@gmail.com>
2026-02-14 21:22:24 -0300

    first commit

--- pablo/course/fastapi/microservices/libros/main.py
+++ pablo/course/fastapi/microservices/libros/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from pablo.course.fastapi.microservices.libros.config.db import engine, Base
 from pablo.course.fastapi.microservices.libros.routers import libros
-import pablo.course.fastapi.microservices.libros.entities
+#import pablo.course.fastapi.microservices.libros.entities
 
 app = FastAPI()
 Base.metadata.create_all(engine)
