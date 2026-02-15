e26474b5c36216ca481e8eaaf06e3548a121b442
simon <pablo.ralil.dev@gmail.com>
2026-02-14 22:15:30 -0300

    first commit

--- pablo/course/fastapi/microservices/libros/main.py
+++ pablo/course/fastapi/microservices/libros/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 
 from pablo.course.fastapi.microservices.libros.config.db import engine, Base
@@ -5,5 +6,13 @@ from pablo.course.fastapi.microservices.libros.routers import libros
 #import pablo.course.fastapi.microservices.libros.entities
 
 app = FastAPI()
-Base.metadata.create_all(engine)
+
+# Solo crea las tablas si la variable INIT_DB es "true"
+if os.getenv("INIT_DB", "false").lower() == "true":
+    print("LOG: Ejecutando creación de tablas...")
+    Base.metadata.create_all(bind=engine)
+else:
+    print("LOG: Saltando creación de tablas (Modo producción)")
+
+#Base.metadata.create_all(engine)
 app.include_router(libros.router, prefix="/libros", tags=["libros"])
